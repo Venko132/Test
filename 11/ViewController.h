92a3b38b45910b5f0f14eed7d58c9fca216283cb
Venko132 <vasya@Vasyas-Mac.local>
2013-12-27 14:55:07 +0200

    sdfsfsd

---
+++ 11/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  11
+//
+//  Created by Vasya on 12.11.13.
+//  Copyright (c) 2013 Vasya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imgAnimation;
+
+@end
---
+++ 11/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  11
+//
+//  Created by Vasya on 11.11.13.
+//  Copyright (c) 2013 Vasya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([_AppDelegate class]));
+    }
+}
